21e6dc9f004f4b1a0c3f6a4d74e062e17fcc788c
Bovis <Paul.Bovis2@ec.gc.ca>
2020-06-15 10:22:08 -0600

    Change dropdown nav links to relative

--- resources/js/components/nav/NavDropdown.vue
+++ resources/js/components/nav/NavDropdown.vue
@@ -17,30 +17,30 @@
             style="top: 50px;"
             v-if="isOpen"
         >
-            <span class="pb-2 px-3">Logged in as: <a :href="`/users/${authUser.id}`"><strong>{{ authUser.name }}</strong></a></span>  
+            <span class="pb-2 px-3">Logged in as: <a :href="`users/${authUser.id}`"><strong>{{ authUser.name }}</strong></a></span>  
 
             <hr class="border-t border-gray-300">
 
-            <a class="text-gray-900 py-2 px-3" href="/logbooks">Logbooks</a>
+            <a class="text-gray-900 py-2 px-3" href="logbooks">Logbooks</a>
 
             <template v-if="hasRole(['administrator'])">
-                <a class="text-gray-900 py-2 px-3" href="/users">Manage users</a>
+                <a class="text-gray-900 py-2 px-3" href="users">Manage users</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/levels">Manage levels</a>
+                <a class="text-gray-900 py-2 px-3" href="levels">Manage levels</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/lessons">Manage lessons</a>
+                <a class="text-gray-900 py-2 px-3" href="lessons">Manage lessons</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/lesson-versions">Manage lesson versions</a>
+                <a class="text-gray-900 py-2 px-3" href="lesson-versions">Manage lesson versions</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/objectives">Manage objectives</a>
+                <a class="text-gray-900 py-2 px-3" href="objectives">Manage objectives</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/recommendations">Manage recommendations</a>
+                <a class="text-gray-900 py-2 px-3" href="recommendations">Manage recommendations</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/statuses">Manage statuses</a>
+                <a class="text-gray-900 py-2 px-3" href="statuses">Manage statuses</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/logbook-categories">Manage logbook categories</a>
+                <a class="text-gray-900 py-2 px-3" href="logbook-categories">Manage logbook categories</a>
 
-                <a class="text-gray-900 py-2 px-3" href="/reports">Reports</a>
+                <a class="text-gray-900 py-2 px-3" href="reports">Reports</a>
             </template>
 
             <hr class="border-t border-gray-300">
