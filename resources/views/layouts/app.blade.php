82e7d668639673500082d3a6b74e8bf28f8410c4
Bovis <Paul.Bovis2@ec.gc.ca>
2020-06-15 16:38:16 -0600

    add es6-promise polyfill

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.js" defer></script> 
+
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
