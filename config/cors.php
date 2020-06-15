5b2ef559d6cb8dcf7f11868caac266b854f6a511
Paul Bovis <bovisp@msc-educ-smc.cmc.ec.gc.ca>
2020-06-15 21:13:53 +0000

    Change cors config

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'api/users/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:8000', 'http://msc-educ-smc.cmc.ec.gc.ca'],
 
     'allowed_origins_patterns' => [],
 
