6ff4a12374e09a6fd571ec36236f64852e4ce376
Paul Bovis <paulbovisblahblah@gmail.com>
2023-02-20 10:35:45 -0700

    Modify Vue2-Editor in PackageEvaluationDetails.vue  fixed3

--- resources/js/pages/userpackage/components/PackageEvaluationDetails.vue
+++ resources/js/pages/userpackage/components/PackageEvaluationDetails.vue
@@ -84,7 +84,7 @@ export default {
                 [{header: [1, 2, 3, 4, 5, 6, false] }],
                 ["bold", "italic", "underline"],
                 [{script: 'sub'}, {script: 'super'}],
-                [{align: false}, {align: 'center'}, {align: 'right'}],
+                [{align: []}],
                 [{ list: "ordered" }, { list: "bullet" }],
                 [{indent: '-1'}, {indent: '+1'}],
                 ["link", "image"]
